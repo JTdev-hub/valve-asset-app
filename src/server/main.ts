e097b1c855013beed9468d4790000ae80b92c41d
Ranel <jrrtuble@gmail.com>
2025-04-22 12:37:58 +0800

    latest commit

--- src/server/main.ts
+++ src/server/main.ts
@@ -10,7 +10,7 @@ const prisma = new PrismaClient();
 app.use(express.json());
 app.use(cors());
 
-app.get("/customers", async (req, res) => {
+app.get("/api/customers", async (req, res) => {
   const { id } = req.query;
   const customers = await prisma.customers.findMany({
     where: id ? { id: Number(id) } : undefined,
